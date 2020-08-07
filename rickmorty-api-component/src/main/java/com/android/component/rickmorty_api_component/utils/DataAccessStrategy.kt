92acfce5248ad0ae8290d01c80430f67afac5ef4
baylonedward <baylonedward@gmail.com>
2020-08-08 00:47:19 +0800

    🛠 Fix performGetOperation()

--- rickmorty-api-component/src/main/java/com/android/component/rickmorty_api_component/utils/DataAccessStrategy.kt
+++ rickmorty-api-component/src/main/java/com/android/component/rickmorty_api_component/utils/DataAccessStrategy.kt
@@ -2,11 +2,9 @@ package com.android.component.rickmorty_api_component.utils
 
 import com.android.component.rickmorty_api_component.utils.Resource.Status.ERROR
 import com.android.component.rickmorty_api_component.utils.Resource.Status.SUCCESS
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.emitAll
-import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.*
 
 /**
  * Created by: ebaylon.
@@ -18,17 +16,13 @@ fun <T, A> performGetOperation(
   databaseQuery: () -> Flow<T>,
   networkCall: suspend () -> Resource<A>,
   saveCallResult: suspend (A) -> Unit
-): Flow<Resource<T>> =
-  flow {
-    emit(Resource.loading())
-    val source = databaseQuery.invoke().map { Resource.success(it) }
-    emitAll(source)
-
-    val responseStatus = networkCall.invoke()
-    if (responseStatus.status == SUCCESS) {
-      responseStatus.data?.also { saveCallResult(it) }
-    } else if (responseStatus.status == ERROR) {
-      emit(Resource.error(responseStatus.message!!))
-      emitAll(source)
-    }
+): Flow<Resource<T>> = flow {
+  emit(Resource.loading())
+  val source = databaseQuery.invoke().map { Resource.success(it) }
+  val responseStatus = networkCall.invoke()
+  when (responseStatus.status) {
+    SUCCESS -> responseStatus.data?.also { saveCallResult(it) }
+    ERROR -> emit(Resource.error(responseStatus.message!!))
   }
+  emitAll(source)
+}.flowOn(Dispatchers.IO)
